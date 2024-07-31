9d0d0d8f78b444edb36a01b6f20754a95686b19d
Bhargav41359 <bhargav.r413@gmail.com>
2024-07-31 17:26:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,31 @@
+
 provider "aws" {
   access_key = "AKIAW5IZRWOYXSOWJSNG"
   secret_key = "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"
   region = "us-east-1"
-
 }
 
-resource "aws_vpc" "dep2" {
+resource "aws_vpc" "dep1" {
     cidr_block = "192.168.0.0/16"
     tags = {
-      "Name" = "VPC3333"
+      "Name" = "BH1"
       
     }
+    depends_on = [ aws_vpc.dep3 ]
+  
 }
+resource "aws_vpc" "dep2" {
+    cidr_block = "192.169.0.0/16"
+    tags = {
+      "Name" = "BH2"
+      
+    }
+    depends_on = [ aws_vpc.dep3 ]
+  
+}
+resource "aws_vpc" "dep3" {
+    cidr_block = "192.170.0.0/16"
+    tags = {
+      "Name" = "BH3"
+      
+    }
